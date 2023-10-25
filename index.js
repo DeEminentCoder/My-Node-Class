00ea95d69ebf26cf3b7e716ed0c588e80c482d04
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-10-25 15:35:45 +0100

    node

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-app.get("/",(req,resp)=>{
-
-})
-app.listen(5000,(err)=>{
-    if(err){
-        console.log("Something went wrong");
-    }
-    console.log("Yess");
-})
+app.get("/", (req, res) => {
+  res.send();
+});
+app.listen(5000, (err) => {
+  if (err) {
+    console.log("Something went wrong");
+  }
+  console.log("Yess");
+});
