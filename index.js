aae666e6f53642eb51067edde2d99294efc44933
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-10-26 12:45:42 +0100

    app.get

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-  res.send("no err");
+  res.send([{ fn: "Sola", ln: "Kunle" }]);
+});
+app.get("/about", (req, res) => {
+  res.send("No error");
 });
 
 app.listen(5000, (err) => {
