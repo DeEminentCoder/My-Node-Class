7a175978f89171d514dce5a06bdf3fff2c56d3be
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-10-29 14:24:07 +0100

    index

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app = express();
+const ejs = require("ejs");
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   // res.send([{ fn: "Sola", ln: "Kunle" }]);
-  res.sendFile(__dirname+"/index.html")
+  // res.sendFile(__dirname+"/index.html")
+  res.render("index", {
+    myName: "Kun",
+    allstudents: [
+      { firstname: "Kun", lastname: "Saade" },
+      { firstname: "Tola", lastname: "wayne" },
+      { firstname: "Tom", lastname: "Vic" },
+    ],
+  });
 });
 app.get("/about", (req, res) => {
   res.send("No error");
+});
+app.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
 app.listen(5000, (err) => {
