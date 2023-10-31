6aa875db19d2f863e0bd8c1011ffe872747baa5a
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-10-31 12:56:28 +0100

    omit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   // res.send([{ fn: "Sola", ln: "Kunle" }]);
   // res.sendFile(__dirname+"/index.html")
+ 
   res.render("index", {
     myName: "Kun",
     allstudents: [
