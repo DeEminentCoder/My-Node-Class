b50ad7620d941de541d5c7ef5ecf2684dd90b0c5
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-11-15 09:53:49 +0100

    ls

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
   res.render("index", {
     myName: "Kun",
     allstudents: [
-      { firstname: "Kun", lastname: "Saaade" },
+      { firstname: "Kun", lastname: "Saade" },
       { firstname: "Tola", lastname: "wayne" },
       { firstname: "Tom", lastname: "Vic" },
     ],
